ec8b5c5e6563d8327cef30c679461cf238c1c54a
Michael Cittar <mcittar@gmail.com>
2017-05-24 14:41:07 -0700

    Button challenge

---
+++ broken.js
@@ -0,0 +1,55 @@
+class DistanceTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      distanceTravelled: 0,
+      speed: 1,
+    };
+    this.interval;
+  }
+
+  componentDidMount() {
+    this.startTracker();
+  }
+
+  clearInterval(this.interval):
+
+  startTracker() {
+    this.interval = setInterval(() => {
+      this.updateDistance();
+    }, 1000);
+  }
+
+  updateDistance() {
+    const { distanceTravelled, speed } = this.state;
+    const updatedDistance = distanceTravelled + speed;
+    console.log(`Before update, distance was: ${this.state.distanceTravelled}`);
+    this.setState({
+      distanceTravelled: updatedDistance,
+    });
+    console.log(`After update, distance is: ${this.state.distanceTravelled}`);
+  }
+
+  updateSpeed(value) {
+    this.setState({
+      speed: this.state.speed + value,
+    });
+  }
+
+  render() {
+    const { children, headerText } = this.props;
+
+    return (
+      <div>
+        <h2>{headerText}</h2>
+        <h3>You are moving north at {this.state.speed} feet per second.</h3>
+        <h3>You have travelled north {this.state.distanceTravelled} feet so far.</h3>
+        <div>
+          <button onClick={updateSpeed(1)}>Go Faster</button>
+          <button onClick={updateSpeed(-1)}>Go Slower</button>
+        </div>
+        {children}
+      </div>
+    );
+  }
+}
